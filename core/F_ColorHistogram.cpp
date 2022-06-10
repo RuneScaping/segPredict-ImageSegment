29b1dee12833cab438f4282939b4980e8d2e97d5
RuneScaping <runescapop@proton.me>
2022-06-10 14:12:29 +0000

    fix testcase

--- core/F_ColorHistogram.cpp
+++ core/F_ColorHistogram.cpp
@@ -149,4 +149,98 @@ bool F_ColorHistogram::getFeatureVectorForOneSupernode(osvm_node *x, Slice* slic
   case INCLUDE_NEIGHBORS_IN_SEPARATE_BINS:
     {
       for(vector < supernode* >::iterator itN = s->neighbors.begin();
-          itN != s->neig
+          itN != s->neighbors.end();itN++) {
+        sn = *itN;
+        double binValue = 1.0/(double)(2*s->neighbors.size()*sn->size());
+        nodeIterator ni = sn->getIterator();
+        ni.goToBegin();
+        while(!ni.isAtEnd()) {
+          ni.get(n);
+          ni.next();
+            
+          idx = 0;
+          // hue and saturation are binned together
+          for(int c=0;c<2;c++) {
+            pValue = (int)(((uchar*)(_img->imageData + n.y*_img->widthStep))[n.x*_img->nChannels+c]);
+            idx = idx*nBinsPerSupernode + (pValue*valToBin);
+          }
+          hist.histData[offsetNeighbors+idx]+=binValue;
+
+          // value is binned separately
+          pValue = (int)(((uchar*)(_img->imageData + n.y*_img->widthStep))[n.x*_img->nChannels+2]);
+          idx = nBinsPerSupernode2 + (pValue*valToBin);
+          hist.histData[offsetNeighbors+idx]+=binValue;
+        }
+      }
+    }
+    break;
+  case INCLUDE_NEIGHBORS_PLUS_LOCATION:
+    {
+      node cs;
+      node cn;
+      float v[2];
+      int angleXY;
+      int angleIdx;
+      int offsetAngle;
+      s->getCenter(cs);
+
+      for(vector < supernode* >::iterator itN = s->neighbors.begin();
+          itN != s->neighbors.end();itN++) {
+        sn = *itN;
+
+        sn->getCenter(cn);
+        // compute vector sn
+        v[0] = cs.x - cn.x;
+        v[1] = cs.y - cn.y;
+        //v[2] = cs.z - cn.z;
+                    
+        angleXY = atan2(v[1], v[0])*180.0/PI;
+        angleXY += 180.0;
+        //angleIdx = angleToIdx(angleXY);
+        angleIdx = angleXY*nLocations/360.0;
+        if(angleIdx >= nLocations) {
+          angleIdx = nLocations - 1;
+        }
+        offsetAngle = offsetNeighbors + (angleIdx*nBins);
+
+        double binValue = 1.0/(double)((nLocations+1)*s->neighbors.size()*sn->size());
+        nodeIterator ni = sn->getIterator();
+        ni.goToBegin();
+        while(!ni.isAtEnd()) {
+          ni.get(n);
+          ni.next();
+            
+          idx = 0;
+          // hue and saturation are binned together
+          for(int c=0;c<2;c++) {
+            pValue = (int)(((uchar*)(_img->imageData + n.y*_img->widthStep))[n.x*_img->nChannels+c]);
+            idx = idx*nBinsPerSupernode + (pValue*valToBin);
+          }
+          hist.histData[offsetAngle+idx]+=binValue;
+
+          // value is binned separately
+          pValue = (int)(((uchar*)(_img->imageData + n.y*_img->widthStep))[n.x*_img->nChannels+2]);
+          idx = nBinsPerSupernode2 + (pValue*valToBin);
+          hist.histData[offsetAngle+idx]+=binValue;
+        }
+      }
+    }
+    break;
+  default:
+    break;
+  }
+
+  for(int i = 0;i < hist.nBins; i++) {
+    x[i].value = hist.histData[i];
+  }
+  
+  return true;
+}
+
+bool F_ColorHistogram::getFeatureVectorForOneSupernode(osvm_node *x, Slice3d* slice3d, int supernodeId)
+{
+  printf("[F_ColorHistogram] Error : getFeatureVector(osvm_node *x, Slice3d& slice3d, int supernodeId) not implemented\n");
+  exit(-1);
+
+  return false;
+}

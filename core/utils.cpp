e795ba5cc7a5e39949f96726ae4615bf50645339
RuneScaping <runescapop@proton.me>
2022-07-29 05:51:56 +0000

    Formatting changes.

--- core/utils.cpp
+++ core/utils.cpp
@@ -334,4 +334,378 @@ int compareBWImages(IplImage *ptrModel,
                     IplImage *ptrImg,
                     float& true_neg,
                     float& true_pos,
-                    
+                    float& false_neg,
+                    float& false_pos,
+                    bool normalize)
+{
+  const int POS_VALUE = 255;
+  //const int NEG_VALUE = 0;
+  uchar* ptrM;
+  uchar* ptrI;
+
+  if((ptrModel->width != ptrImg->width) || (ptrModel->height != ptrImg->height)
+     || (ptrModel->nChannels != ptrImg->nChannels))
+    {
+      printf("[utils] Error : The 2 images have different attributes (width, height, number of channels)\n");
+      return -1;
+    }
+
+  int total_pos = 0;
+  int total_neg = 0;
+  true_pos = 0.0f;
+  true_neg = 0.0f;
+  false_pos = 0.0f;
+  false_neg = 0.0f;
+  for(int u=0;u<ptrImg->width;u++)
+    {
+      for(int v=0;v<ptrImg->height;v++)
+        {
+          ptrM = &((uchar*)(ptrModel->imageData + ptrModel->widthStep*v))[u*ptrModel->nChannels];
+          ptrI = &((uchar*)(ptrImg->imageData + ptrImg->widthStep*v))[u*ptrImg->nChannels];
+
+          //printf("%d %d ",(int)ptrC,(int)ptrC2);
+
+          if(ptrImg->nChannels == 3)
+            {
+              // color image
+              if((*ptrM == POS_VALUE) && (*(ptrM+1) == POS_VALUE) && (*(ptrM+2) == POS_VALUE))
+                {
+                  // positive sample
+                  total_pos++;
+
+                  if((*ptrI == POS_VALUE) && (*(ptrI+1) == POS_VALUE) && (*(ptrI+2) == POS_VALUE))
+                    true_pos++;
+                  else
+                    false_neg++;
+                }
+              else
+                {
+                  // negative sample
+                  total_neg++;
+
+                  if((*ptrI == POS_VALUE) && (*(ptrI+1) == POS_VALUE) && (*(ptrI+2) == POS_VALUE))
+                    false_pos++;
+                  else
+                    true_neg++;
+                }
+            }
+          else
+            {
+              // gray image
+              if(*ptrM == POS_VALUE)
+                {
+                  total_pos++;
+
+                  if(*ptrI == POS_VALUE)
+                    true_pos++;
+                  else
+                    false_neg++;
+                }
+              else
+                {
+                  total_neg++;
+
+                  if(*ptrI == POS_VALUE)
+                    false_pos++;
+                  else
+                    true_neg++;
+                }
+            }
+        }
+    }
+
+  //printf("Comparison %f %f %d %d\n",true_pos,false_neg,total_pos,total_neg);
+  if(normalize)
+    {
+      if(total_pos != 0)
+        {
+          true_pos *= (100.0f/total_pos);
+          false_neg *= (100.0f/total_pos);
+        }
+      if(total_neg != 0)
+        {
+          true_neg *= (100.0f/total_neg);
+          false_pos *= (100.0f/total_neg);
+        }
+    }
+
+  return 0;
+}
+
+void save16bitsImage(const char* filename, IplImage* img)
+{
+  ofstream ofs(filename, ios::out | ios::binary);
+  ushort* ptrImg;
+  for(int y=0;y<img->height;y++) {
+    for(int x=0;x<img->width;x++) {
+      ptrImg = ((ushort*)(img->imageData + img->widthStep*y)) + x*(img)->nChannels;
+      ofs.write((char*)ptrImg,sizeof(ushort));
+    }
+  }
+  ofs.close();
+}
+
+void save32bitsImage(const char* filename, IplImage* img)
+{
+  ofstream ofs(filename, ios::out | ios::binary);
+  uint* ptrImg;
+  for(int y=0;y<img->height;y++) {
+    for(int x=0;x<img->width;x++) {
+      ptrImg = ((uint*)(img->imageData + img->widthStep*y)) + x*(img)->nChannels;
+      ofs.write((char*)ptrImg,sizeof(int));
+    }
+  }
+  ofs.close();
+}
+
+IplImage* load32bitsImage(const char* filename, CvSize& size)
+{
+  IplImage* img = cvCreateImage(size, IPL_DEPTH_32S, 1);
+  ifstream ifs(filename, ios::in | ios::binary);
+
+  int* ptrImg;
+  for(int y=0;y<img->height;y++)
+    for(int x=0;x<img->width;x++)
+      {
+        ptrImg = ((int*)(img->imageData + img->widthStep*y)) + x*(img)->nChannels;
+        ifs.read((char*)ptrImg,sizeof(int));
+      }
+  ifs.close();
+  return img;
+}
+
+void saveDoubleImage(const char* filename, IplImage* img)
+{
+  ofstream ofs(filename, ios::out | ios::binary);
+
+  double* ptrImg;
+  // channels first (easier to load in matlab)
+  for(int c = 0; c < img->nChannels; c++)
+    for(int y=0;y<img->height;y++)
+      for(int x=0;x<img->width;x++)
+        {
+          ptrImg = ((double*)(img->imageData + img->widthStep*y)) + x*(img)->nChannels + c;
+          ofs.write((char*)ptrImg,sizeof(double));
+        }
+  ofs.close();
+}
+
+IplImage* loadDoubleImage(const char* filename, CvSize& size, int nChannels)
+{
+  IplImage* img = cvCreateImage(size, IPL_DEPTH_64F, nChannels);
+  ifstream ifs(filename, ios::in | ios::binary);
+
+  double* ptrImg;
+  for(int c = 0; c < nChannels; c++)
+    for(int y=0;y<img->height;y++)
+      for(int x=0;x<img->width;x++)
+        {
+          ptrImg = ((double*)(img->imageData + img->widthStep*y)) + x*(img)->nChannels + c;
+          ifs.read((char*)ptrImg,sizeof(double));
+        }
+  ifs.close();
+  return img;
+}
+
+void saveFloatImage(const char* filename, IplImage* img)
+{
+  ofstream ofs(filename, ios::out | ios::binary);
+
+  float* ptrImg;
+  // channels first (easier to load in matlab)
+  for(int c = 0; c < img->nChannels; c++)
+    for(int y=0;y<img->height;y++)
+      for(int x=0;x<img->width;x++)
+        {
+          ptrImg = ((float*)(img->imageData + img->widthStep*y)) + x*(img)->nChannels + c;
+          ofs.write((char*)ptrImg,sizeof(float));
+        }
+  ofs.close();
+}
+
+IplImage* loadFloatImage(const char* filename, CvSize& size, int nChannels)
+{
+  IplImage* img = cvCreateImage(size, IPL_DEPTH_64F, nChannels);
+  ifstream ifs(filename, ios::in | ios::binary);
+
+  float* ptrImg;
+  for(int c = 0; c < nChannels; c++)
+    for(int y=0;y<img->height;y++)
+      for(int x=0;x<img->width;x++)
+        {
+          ptrImg = ((float*)(img->imageData + img->widthStep*y)) + x*(img)->nChannels + c;
+          ifs.read((char*)ptrImg,sizeof(float));
+        }
+  ifs.close();
+  return img;
+}
+
+void saveImage(const char* filename, IplImage* img, const char* ext)
+{
+  const char* raw_ext = ".raw";
+  IplImage* img8U = 0;
+  bool releaseImage = true;
+  if(img->depth == IPL_DEPTH_64F)
+    {
+      stringstream sout;
+      sout << filename << "D" << raw_ext;
+      saveDoubleImage(sout.str().c_str(),img);
+      double2ucharImage(img, img8U);
+    }
+  else if(img->depth == IPL_DEPTH_32F)
+    {
+      stringstream sout;
+      sout << filename << "F" << raw_ext;
+      saveFloatImage(sout.str().c_str(),img);
+      float2ucharImage(img, img8U);
+    }
+  else if(img->depth == (int)IPL_DEPTH_32S)
+    {
+      stringstream sout;
+      sout << filename << "S" << raw_ext;
+      save32bitsImage(sout.str().c_str(),img);
+    }
+  else if(img->depth == IPL_DEPTH_8U)
+    {
+      img8U = img;
+      releaseImage = false;
+    }
+  else
+    {
+      printf("[utils] saveImage : unknown image type\n");
+    }
+
+  if(img8U != 0)
+    {
+      stringstream sout;
+      sout << filename << "U" << ext;
+      cvSaveImage(sout.str().c_str(),img8U);
+      if(releaseImage)
+        cvReleaseImage(&img8U);
+    }
+}
+
+void double2ucharImage(IplImage* imgIn, IplImage*& imgOut)
+{
+  double vImg;
+  double maxValue = 0;
+  imgOut = cvCreateImage(cvSize(imgIn->width,imgIn->height), IPL_DEPTH_8U, imgIn->nChannels);
+  for(int c = 0; c < imgIn->nChannels; c++)
+    for(int y=0;y<imgIn->height;y++)
+      for(int x=0;x<imgIn->width;x++)
+        {
+          vImg = ((double*)(imgIn->imageData + imgIn->widthStep*y))[x*imgIn->nChannels + c];
+          if(maxValue < vImg)
+            maxValue = vImg;
+        }
+
+  //printf("double2ucharImage %f\n",maxValue);
+  cvConvertScale(imgIn,imgOut,255.0/maxValue);
+}
+
+void float2ucharImage(IplImage* imgIn, IplImage*& imgOut)
+{
+  float vImg;
+  float maxValue = 0;
+  imgOut = cvCreateImage(cvSize(imgIn->width,imgIn->height), IPL_DEPTH_8U, imgIn->nChannels);
+  for(int c = 0; c < imgIn->nChannels; c++)
+    for(int y=0;y<imgIn->height;y++)
+      for(int x=0;x<imgIn->width;x++)
+        {
+          vImg = ((float*)(imgIn->imageData + imgIn->widthStep*y))[x*imgIn->nChannels + c];
+          if(maxValue < vImg)
+            maxValue = vImg;
+        }
+
+  //printf("double2ucharImage %f\n",maxValue);
+  cvConvertScale(imgIn,imgOut,255.0/maxValue);
+}
+
+int sign(int v)
+{
+  return v > 0 ? 1 : (v < 0 ? -1 : 0);
+}
+
+uint time_seed()
+{
+  time_t now = time ( 0 );
+  unsigned char *p = (unsigned char *)&now;
+  unsigned seed = 0;
+  size_t i;
+ 
+  for ( i = 0; i < sizeof now; i++ )
+    seed = seed * ( UCHAR_MAX + 2U ) + p[i];
+ 
+  return seed;
+}
+
+void exportCube(uchar* rawData,
+                const char* filename,
+                int cubeDepth,
+                int cubeHeight,
+                int cubeWidth)
+{
+#ifdef USE_ITK
+  exportTIFCube(rawData,
+                filename,
+                cubeDepth,
+                cubeHeight,
+                cubeWidth);
+#else
+  exportVIVACube(rawData,
+                 filename,
+                 cubeDepth,
+                 cubeHeight,
+                 cubeWidth);
+#endif
+}
+
+void exportCube(float* rawData,
+                const char* filename,
+                int cubeDepth,
+                int cubeHeight,
+                int cubeWidth)
+{
+  exportVIVACube(rawData,
+                 filename,
+                 cubeDepth,
+                 cubeHeight,
+                 cubeWidth);
+}
+
+bool getGroundTruthName(string& groundTruthName, const string& maskDir, const string& filename)
+{
+  bool found_file = false;
+  vector<string> list_ext;
+  list_ext.push_back(".png");
+  list_ext.push_back(".bmp");
+  list_ext.push_back("_GT.bmp");
+  list_ext.push_back(".jpg");
+  list_ext.push_back(".tif");
+  list_ext.push_back(".labels");
+
+  string baseName = getNameFromPathWithoutExtension(filename);
+  for(vector<string>::const_iterator it = list_ext.begin();
+      it != list_ext.end(); ++it) {
+    groundTruthName = maskDir + baseName + *it;
+    if(fileExists(groundTruthName.c_str())) {
+      found_file = true;
+      break;      
+    }
+  }
+  return found_file;
+}
+
+//---------------------------------------------------------------------------ITK
+
+#ifdef USE_ITK
+
+void exportTIFCube(uchar* rawData,
+                   const char* filename,
+                   int cubeDepth,
+                   int cubeHeight,
+                   int cubeWidth)
+{
+  // import data to an itk image
+  const int dimension = 3;
+  ty

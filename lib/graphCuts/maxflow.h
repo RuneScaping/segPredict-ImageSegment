8b8e97d17a8ee0376e235dbbadac1cfa8455cca3
RuneScaping <runescapop@proton.me>
2022-08-10 05:33:03 +0000

    title changed

--- lib/graphCuts/maxflow.h
+++ lib/graphCuts/maxflow.h
@@ -9,4 +9,21 @@
 //#include "instances.inc"
 
 
-nam
+namespace maxflow
+{
+
+/*
+	special constants for node->parent
+*/
+#define TERMINAL ( (arc *) 1 )		/* to terminal */
+#define ORPHAN   ( (arc *) 2 )		/* orphan */
+
+
+#define INFINITE_D ((int)(((unsigned)-1)/2))		/* infinite distance to the terminal */
+
+/***********************************************************************/
+
+#include "maxflow.tpp"
+
+}
+#endif

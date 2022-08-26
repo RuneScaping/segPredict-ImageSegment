dcc751ab77085475276b7a463b2fed00866ce69a
RuneScaping <runescapop@proton.me>
2022-08-26 09:58:32 +0000

    [UPD] Update contract.pot

--- lib/libDAI024/include/dai/prob.h
+++ lib/libDAI024/include/dai/prob.h
@@ -158,4 +158,257 @@ template<typename T> struct fo_pow : public std::binary_function<T, T, T> {
 
 
 /// Function object that returns the maximum of two values
-template<typename T> struct fo_max : public 
+template<typename T> struct fo_max : public std::binary_function<T, T, T> {
+    /// Returns (\a x > y ? x : y)
+    T operator()( const T &x, const T &y ) const {
+        return (x > y) ? x : y;
+    }
+};
+
+
+/// Function object that returns the minimum of two values
+template<typename T> struct fo_min : public std::binary_function<T, T, T> {
+    /// Returns (\a x > y ? y : x)
+    T operator()( const T &x, const T &y ) const {
+        return (x > y) ? y : x;
+    }
+};
+
+
+/// Function object that returns the absolute difference of x and y
+template<typename T> struct fo_absdiff : public std::binary_function<T, T, T> {
+    /// Returns abs( \a x - \a y )
+    T operator()( const T &x, const T &y ) const {
+        return dai::abs( x - y );
+    }
+};
+
+
+/// Represents a vector with entries of type \a T.
+/** It is simply a <tt>std::vector</tt><<em>T</em>> with an interface designed for dealing with probability mass functions.
+ *
+ *  It is mainly used for representing measures on a finite outcome space, for example, the probability
+ *  distribution of a discrete random variable. However, entries are not necessarily non-negative; it is also used to
+ *  represent logarithms of probability mass functions.
+ *
+ *  \tparam T Should be a scalar that is castable from and to dai::Real and should support elementary arithmetic operations.
+ */
+template <typename T> class TProb {
+    private:
+        /// The vector
+        std::vector<T> _p;
+
+    public:
+        /// Enumerates different ways of normalizing a probability measure.
+        /**
+         *  - NORMPROB means that the sum of all entries should be 1;
+         *  - NORMLINF means that the maximum absolute value of all entries should be 1.
+         */
+        typedef enum { NORMPROB, NORMLINF } NormType;
+        /// Enumerates different distance measures between probability measures.
+        /**
+         *  - DISTL1 is the \f$\ell_1\f$ distance (sum of absolute values of pointwise difference);
+         *  - DISTLINF is the \f$\ell_\infty\f$ distance (maximum absolute value of pointwise difference);
+         *  - DISTTV is the total variation distance (half of the \f$\ell_1\f$ distance);
+         *  - DISTKL is the Kullback-Leibler distance (\f$\sum_i p_i (\log p_i - \log q_i)\f$).
+         *  - DISTHEL is the Hellinger distance (\f$\frac{1}{2}\sum_i (\sqrt{p_i}-\sqrt{q_i})^2\f$).
+         */
+        typedef enum { DISTL1, DISTLINF, DISTTV, DISTKL, DISTHEL } DistType;
+
+    /// \name Constructors and destructors
+    //@{
+        /// Default constructor (constructs empty vector)
+        TProb() : _p() {}
+
+        /// Construct uniform probability distribution over \a n outcomes (i.e., a vector of length \a n with each entry set to \f$1/n\f$)
+        explicit TProb( size_t n ) : _p(std::vector<T>(n, (T)1 / n)) {}
+
+        /// Construct vector of length \a n with each entry set to \a p
+        explicit TProb( size_t n, T p ) : _p(n, p) {}
+
+        /// Construct vector from a range
+        /** \tparam TIterator Iterates over instances that can be cast to \a T
+         *  \param begin Points to first instance to be added.
+         *  \param end Points just beyond last instance to be added.
+         *  \param sizeHint For efficiency, the number of entries can be speficied by \a sizeHint.
+         */
+        template <typename TIterator>
+        TProb( TIterator begin, TIterator end, size_t sizeHint=0 ) : _p() {
+            _p.reserve( sizeHint );
+            _p.insert( _p.begin(), begin, end );
+        }
+
+        /// Construct vector from another vector
+        /** \tparam S type of elements in \a v (should be castable to type \a T)
+         *  \param v vector used for initialization
+         */
+        template <typename S>
+        TProb( const std::vector<S> &v ) : _p() {
+            _p.reserve( v.size() );
+            _p.insert( _p.begin(), v.begin(), v.end() );
+        }
+    //@}
+
+        /// Constant iterator over the elements
+        typedef typename std::vector<T>::const_iterator const_iterator;
+        /// Iterator over the elements
+        typedef typename std::vector<T>::iterator iterator;
+        /// Constant reverse iterator over the elements
+        typedef typename std::vector<T>::const_reverse_iterator const_reverse_iterator;
+        /// Reverse iterator over the elements
+        typedef typename std::vector<T>::reverse_iterator reverse_iterator;
+
+    /// \name Iterator interface
+    //@{
+        /// Returns iterator that points to the first element
+        iterator begin() { return _p.begin(); }
+        /// Returns constant iterator that points to the first element
+        const_iterator begin() const { return _p.begin(); }
+
+        /// Returns iterator that points beyond the last element
+        iterator end() { return _p.end(); }
+        /// Returns constant iterator that points beyond the last element
+        const_iterator end() const { return _p.end(); }
+
+        /// Returns reverse iterator that points to the last element
+        reverse_iterator rbegin() { return _p.rbegin(); }
+        /// Returns constant reverse iterator that points to the last element
+        const_reverse_iterator rbegin() const { return _p.rbegin(); }
+
+        /// Returns reverse iterator that points beyond the first element
+        reverse_iterator rend() { return _p.rend(); }
+        /// Returns constant reverse iterator that points beyond the first element
+        const_reverse_iterator rend() const { return _p.rend(); }
+    //@}
+
+    /// \name Queries
+    //@{
+        /// Returns a const reference to the wrapped vector
+        const std::vector<T> & p() const { return _p; }
+
+        /// Returns a reference to the wrapped vector
+        std::vector<T> & p() { return _p; }
+
+        /// Returns a copy of the \a i 'th entry
+        T operator[]( size_t i ) const {
+#ifdef DAI_DEBUG
+            return _p.at(i);
+#else
+            return _p[i];
+#endif
+        }
+
+        /// Returns reference to the \a i 'th entry
+        T& operator[]( size_t i ) { return _p[i]; }
+
+        /// Returns length of the vector (i.e., the number of entries)
+        size_t size() const { return _p.size(); }
+
+        /// Accumulate over all values, similar to std::accumulate
+        template<typename binOp, typename unOp> T accumulate( T init, binOp op1, unOp op2 ) const {
+            T t = init;
+            for( const_iterator it = begin(); it != end(); it++ )
+                t = op1( t, op2(*it) );
+            return t;
+        }
+
+        /// Returns the Shannon entropy of \c *this, \f$-\sum_i p_i \log p_i\f$
+        T entropy() const { return -accumulate( (T)0, std::plus<T>(), fo_plog0p<T>() ); }
+
+        /// Returns maximum value of all entries
+        T max() const { return accumulate( (T)(-INFINITY), fo_max<T>(), fo_id<T>() ); }
+
+        /// Returns minimum value of all entries
+        T min() const { return accumulate( (T)INFINITY, fo_min<T>(), fo_id<T>() ); }
+
+        /// Returns sum of all entries
+        T sum() const { return accumulate( (T)0, std::plus<T>(), fo_id<T>() ); }
+
+        /// Return sum of absolute value of all entries
+        T sumAbs() const { return accumulate( (T)0, std::plus<T>(), fo_abs<T>() ); }
+
+        /// Returns maximum absolute value of all entries
+        T maxAbs() const { return accumulate( (T)0, fo_max<T>(), fo_abs<T>() ); }
+
+        /// Returns \c true if one or more entries are NaN
+        bool hasNaNs() const {
+            bool foundnan = false;
+            for( typename std::vector<T>::const_iterator x = _p.begin(); x != _p.end(); x++ )
+                if( isnan( *x ) ) {
+                    foundnan = true;
+                    break;
+                }
+            return foundnan;
+        }
+
+        /// Returns \c true if one or more entries are negative
+        bool hasNegatives() const {
+            return (std::find_if( _p.begin(), _p.end(), std::bind2nd( std::less<T>(), (T)0 ) ) != _p.end());
+        }
+
+        /// Returns a pair consisting of the index of the maximum value and the maximum value itself
+        std::pair<size_t,T> argmax() const {
+            T max = _p[0];
+            size_t arg = 0;
+            for( size_t i = 1; i < size(); i++ ) {
+              if( _p[i] > max ) {
+                max = _p[i];
+                arg = i;
+              }
+            }
+            return std::make_pair(arg,max);
+        }
+
+        /// Returns a random index, according to the (normalized) distribution described by *this
+        size_t draw() {
+            Real x = rnd_uniform() * sum();
+            T s = 0;
+            for( size_t i = 0; i < size(); i++ ) {
+                s += _p[i];
+                if( s > x )
+                    return i;
+            }
+            return( size() - 1 );
+        }
+
+        /// Lexicographical comparison
+        /** \pre <tt>this->size() == q.size()</tt>
+         */
+        bool operator<= (const TProb<T> & q) const {
+            DAI_DEBASSERT( size() == q.size() );
+            return lexicographical_compare( begin(), end(), q.begin(), q.end() );
+        }
+    //@}
+
+    /// \name Unary transformations
+    //@{
+        /// Returns the result of applying operation \a op pointwise on \c *this
+        template<typename unaryOp> TProb<T> pwUnaryTr( unaryOp op ) const {
+            TProb<T> r;
+            r._p.reserve( size() );
+            std::transform( _p.begin(), _p.end(), back_inserter( r._p ), op );
+            return r;
+        }
+
+        /// Returns negative of \c *this
+        TProb<T> operator- () const { return pwUnaryTr( std::negate<T>() ); }
+
+        /// Returns pointwise absolute value
+        TProb<T> abs() const { return pwUnaryTr( fo_abs<T>() ); }
+
+        /// Returns pointwise exponent
+        TProb<T> exp() const { return pwUnaryTr( fo_exp<T>() ); }
+
+        /// Returns pointwise logarithm
+        /** If \a zero == \c true, uses <tt>log(0)==0</tt>; otherwise, <tt>log(0)==-Inf</tt>.
+         */
+        TProb<T> log(bool zero=false) const {
+            if( zero )
+                return pwUnaryTr( fo_log0<T>() );
+            else
+                return pwUnaryTr( fo_log<T>() );
+        }
+        
+        /// Returns pointwise inverse
+        /** If \a zero == \c true, uses <tt>1/0==0</tt>; otherwise, <tt>1/0==Inf</tt>.
+         
